c8225ad6eae4acb09cd44714959907c83467e878
Gumballll <erik_blair@outlook.com>
2017-07-21 15:11:09 -0800

    Uploaded main.java

--- src/murdermystery/Main.java
+++ src/murdermystery/Main.java
@@ -6,6 +6,8 @@ import java.util.logging.Level;
 
 import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import murdermystery.game.Game;
@@ -14,16 +16,20 @@ import murdermystery.game.GameManager;
 public class Main extends JavaPlugin {
 	public static String serverPath;
 	public static Integer maxgamesize;
+	public static Plugin self;
+	public static World gamelobby;
 	
 	public void onEnable() {		
 		new EventListener(this);
 		saveDefaultConfig();
 		Main.serverPath = getConfig().getString("serverpath");
 		Main.maxgamesize = getConfig().getInt("gamesize");
+		Main.gamelobby = Bukkit.getWorld(getConfig().getString("gamelobby"));
 		if(serverPath == "default") {
 			Bukkit.getLogger().log(Level.SEVERE, "You must set the root path of the server in order to use Murder Mystery! Murder Mystery has been disabled.");
 			Bukkit.getServer().getPluginManager().disablePlugin(this);
 		}
+		Main.self = this;
 	}
 
 	public void onDisable() {
